02083265c02e13ae2c19c12c46920aca09cab985
Alysson Barros <arb35@yahoo.com.br>
2016-12-04 17:06:21 -0200

    Adicionando testes, app catalog e models categoy e product

--- alawebdjango/core/tests/test_views.py
+++ alawebdjango/core/tests/test_views.py
@@ -7,16 +7,12 @@ from django.core.urlresolvers import reverse
 class IndexViewTestCase(TestCase):
 
     def setUp(self):
-        self.client = Client()
-        self.url = reverse('index')
-
-    def tearDown(self):
-        pass
+        self.response = self.client.get('/')
 
     def test_status_code(self):
-        response = self.client.get(self.url)
-        self.assertEquals(response.status_code, 200)
+        """ GET / must return status code 200 """
+        self.assertEquals(self.response.status_code, 200)
 
     def test_template_used(self):
-        response = self.client.get(self.url)
-        self.assertTemplateUsed(response, 'index.html')
+        """ Must use index.html """
+        self.assertTemplateUsed(self.response, 'index.html')
--- alawebdjango/settings.py
+++ alawebdjango/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'alawebdjango.core',
+    'alawebdjango.catalog',
 ]
 
 MIDDLEWARE = [
